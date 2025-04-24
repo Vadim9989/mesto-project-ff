c43861df655860c3846e7e57495a9a41cbb903fc
Vadim Storozhev <vad.storozhev@yandex.ru>
2025-04-24 11:34:01 +0500

    add index.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,48 @@
 
 // @todo: DOM узлы
 
+const cardTemplate = document.querySelector("#card-template").content;
+const placeList = document.querySelector(".places__list");
+
 // @todo: Функция создания карточки
+
+function createCard(cardData, deleteCard) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true); // клонируем шаблон карты
+
+  //элементы внутри картф
+
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  //установка значения из данных карточки
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  //обработчик удаления карточки
+
+  deleteButton.addEventListener("click", () => {
+    deleteCard(cardElement);
+  });
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
 
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+function addCards() {
+  initialCards.forEach((cardData) => {
+    const cardElement = createCard(cardData, deleteCard);
+    placeList.append(cardElement);
+  });
+}
+
+addCards();
